7e652aa4826486a004f89fcbd1a1b5f0ac3671fc
Nikko <dumriquenikko@gmail.com>
2021-04-13 01:13:31 -0700

    subplots

--- python-serial-multiple.py
+++ python-serial-multiple.py
@@ -9,27 +9,28 @@ port = "/dev/ttyACM0"  #for Linux
 def animate(i):
     #xs = xs[-20:]
     #ys = ys[-20:]
-    currElectrode = 1
     data = ser.read(1) # look for a character from serial port, will wait up to timeout above.
-    for key in ys:
-    	if len(data) > 0: #was there a byte to read? should always be true.
-        # xs.append(time.time()-start)
-        # ys.append(ord(data) * 0.01) # take the value of the byte
-            electrodeVal = ord(data) * 0.01
-            ys[key] = electrodeVal
-            print(currElectrode)
-            print(ys)
+    if len(data) > 0:
+        xs.append(time.time() - start)
+        for key in ys:
+    	    if len(data) > 0: #was there a byte to read? should always be true.
+            # xs.append(time.time()-start)
+            # ys.append(ord(data) * 0.01) # take the value of the byte
+                electrodeVal = ord(data) * 0.01
+                #xs.append(time.time() - start)
+                ys[key].append(electrodeVal)
+                #print(ys)
             
     plt.cla()
     # plt.plot(xs,ys)
     electrodeLabels = ["E1", "E2", "E3", "E4", "E5"]
-    ysvals = [ys[1][-1], ys[2][-1], ys[3][-1], ys[4][-1], ys[5][-1]]
-    subplt[0,0].bar(electrodeLabels, ysvals, align = 'center')
-    subplt[0,1].plt(xs, ys[1])
-    subplt[0,2].plt(xs, ys[2])
-    subplt[0,3].plt(xs, ys[3])
-    subplt[0,4].plt(xs, ys[4])
-    subplt[0,5].plt(xs, ys[5])
+#    ysvals = np.array([ys[1][-1:], ys[2][-1:], ys[3][-1:], ys[4][-1:], ys[5][-1:]])
+ #   subplt[0,0].bar(ys.keys(), ysvals, align = 'center')
+    subplt[1,1].plot(xs, ys[1])
+    subplt[1,2].plot(xs, ys[2])
+    subplt[2,0].plot(xs, ys[3])
+    subplt[2,1].plot(xs, ys[4])
+    subplt[2,2].plot(xs, ys[5])
 
     
 #start our program proper:
@@ -54,9 +55,8 @@ ser.flushInput()
 xs = []
 ys = {1:[], 2:[], 3:[], 4:[], 5:[]} #sensors 
 #ys = dict.fromkeys([1,2,3,4,5])
-times= []  
 start = time.time()
-plt.style.use('fivethirtyeight')
-fig, subplt = plt.subplots(nrows = 1, ncols = 6)
+#plt.style.use('fivethirtyeight')
+fig, subplt = plt.subplots(nrows = 3, ncols = 3)
 ani = anim.FuncAnimation(fig, animate, interval = 10)
 plt.show()
